d864c469a3b605ba9a9c4e93b5ef03aa1023955e
Nikmal8 <kamalcharan404@gmail.com>
2024-08-03 11:49:18 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,2 +1,58 @@
-import streamlit as st
-st.write("hello susheel")
+# load the dataset -> feature extraction -> data visualization -> data cleaning -> train test split
+# -> model building -> model training -> model evaluation -> model saving -> streamlit application deploy
+
+# disable warning
+import warnings
+warnings.filterwarnings('ignore')
+
+import pandas as pd
+
+df = pd.read_csv('https://raw.githubusercontent.com/laxmimerit/All-CSV-ML-Data-Files-Download/master/twitter_sentiment.csv', header=None, index_col=[0])
+df = df[[2,3]].reset_index(drop=True)
+df.columns = ['sentiment', 'text']
+
+df.dropna(inplace=True)
+
+df = df[df['text'].apply(len)>1]
+
+
+# basic feature extraction | 
+import preprocess_kgptalkie as ps
+
+df = ps.get_basic_features(df)
+
+
+# Data Cleaning
+# lowercase, remove url, html, punctuations, retweet
+
+
+df['text'] = df['text'].apply(lambda x: x.lower())
+df['text'] = df['text'].apply(lambda x: ps.remove_urls(x))
+df['text'] = df['text'].apply(lambda x: ps.remove_html_tags(x))
+df['text'] = df['text'].apply(lambda x: ps.remove_special_chars(x))
+df['text'] = df['text'].apply(lambda x: ps.remove_rt(x))
+
+# train test split
+from sklearn.model_selection import train_test_split
+
+X_train, X_test, y_train, y_test = train_test_split(df['text'], df['sentiment'], test_size=0.2, random_state=42)
+
+
+# model building
+from sklearn.feature_extraction.text import TfidfVectorizer
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.pipeline import Pipeline
+
+clf = Pipeline([('tfidf', TfidfVectorizer(stop_words=stopwords)), ('clf', RandomForestClassifier(n_estimators=100, n_jobs=-1))])
+clf.fit(X_train, y_train)
+
+# evaluation
+from sklearn.metrics import accuracy_score
+
+predictions = clf.predict(X_test)
+print(accuracy_score(y_test, predictions))
+
+# save model
+import pickle
+
+pickle.dump(clf, open('twitter_sentiment.pkl', 'wb'))
